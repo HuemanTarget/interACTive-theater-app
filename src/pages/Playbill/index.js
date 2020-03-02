636de16aa7dad2a18bb31e6eb7a3b395ec0e5346
HuemanTarget <38532350+HuemanTarget@users.noreply.github.com>
2020-03-01 20:03:49 -0800

    add time to playbill

--- src/pages/Playbill/index.js
+++ src/pages/Playbill/index.js
@@ -1,3 +1,17 @@
-const Playbill = () => 'Playbill'
+import React from 'react';
 
+const Playbill = () => {
+        var tempDate = new Date();
+        var date = tempDate.getFullYear() + '-' + (tempDate.getMonth()+1) + '-' + tempDate.getDate() +' '+ tempDate.getHours()+':'+ tempDate.getMinutes()+':'+ tempDate.getSeconds();
+        const currDate = "Current Date= "+date;
+        
+    
+    
+    return(
+        <div>
+            <h1>Playbill</h1>
+            <p>{currDate}</p>
+        </div>
+    )
+}
 export default Playbill
