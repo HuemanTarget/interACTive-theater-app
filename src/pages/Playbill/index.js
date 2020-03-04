8425fbc534f80ac118808f3d52f8b777fe0cc7d0
HuemanTarget <38532350+HuemanTarget@users.noreply.github.com>
2020-03-03 17:48:04 -0800

    add flip page and open story

--- src/pages/Playbill/index.js
+++ src/pages/Playbill/index.js
@@ -1,13 +1,52 @@
 import React from 'react';
+import FlipPage from "react-flip-page";
+
+import './style.css';
 
 const Playbill = () => {
 
-    const currTime = () => {
-        var tempTime = new Date();
-        var time = tempTime.getHours()+':'+ tempTime.getMinutes();
-        const currTime = time;
-        return currTime
-    }
+    // const currTime = () => {
+    //     var tempTime = new Date();
+    //     var time = tempTime.getHours()+':'+ tempTime.getMinutes();
+    //     const currTime = time;
+    //     return currTime
+    // }
+
+    const pages = [
+        { 
+            title: "Murder On The Cohort Express", 
+            picture: <img src="https://media.newyorker.com/photos/5a0dbe6e2c4e135599ca7016/master/w_1600%2Cc_limit/171127_r31016.jpg" width="360" alt="computer" />,
+            titletwo: "An InterACTive Murder Mystery" 
+        },
+        { 
+            title: "How This Works", 
+            content: "You will be given a series of questions during the story. Your answers change how the story moves forward. There are twenty seven different story lines available. Can you find them all?" 
+        },
+        { 
+            title: "Cast Of Characters",
+            titletwo: "1). Professor Donk", 
+            contenttwo: "Professor of Child Studies",
+            titlethree: "2). The Sophias",
+            contentthree: "Siamese-Twin Circus Performers",
+            titlefour: "3). Ernie Middleware",
+            contentfour: "Heir to the Sriracha Fortune",
+            titlefive: "4). Bitana The Great",
+            contentfive: "Black Magic Mamma (She made me write this)",
+        },
+        { 
+            title: "Cast Contd",
+            titletwo: "5). DJ Galindo", 
+            contenttwo: "Mixes Massive Beats and Jams",
+            titlethree: "6). Mustang Zane",
+            contentthree: "70's Porn Star with a Massive....You Shut Yo Mouth",
+            titlefour: "7). Blake Johnson",
+            contentfour: "Player / Owner of the Basketball Dynasty Blakers",
+        },
+        { 
+            title: "Story",
+            content: "It was a night much like every other night when seven members of the SEI-69 Cohort were invited on the exclusive Cohort Express for a trip from DTLA to Universal Studios Hollywood. Not knowing who invited them there, the group assembled in the dinning car for a meal fit for royalty, Papa Johns. As the scrumptious meal of cold bread sticks, dried chicken balls, and shriveled pizza were served to the guests. The lights went out and a scream was heard.", 
+        }
+    ];
 
     
         
@@ -15,8 +54,34 @@ const Playbill = () => {
     
     return(
         <div>
-            Playbill
-            <p>{currTime()}</p>
+            <div className="app">
+            <FlipPage
+                className="book"
+                showSwipeHint
+                uncutPages
+                orientation="horizontal"
+                width="440"
+                height="600"
+                pageBackground="#fffdf8"
+                animationDuration="400"
+            >
+            {pages.map(page => (
+            <article style={{ width: "400px ", padding: "10px 20px" }}>
+                <h1>{page.title}</h1>
+                <div>{page.picture}</div>
+                <p>{page.titletwo}</p>
+                <p>{page.contenttwo}</p>
+                <p>{page.titlethree}</p>
+                <p>{page.contentthree}</p>
+                <p>{page.titlefour}</p>
+                <p>{page.contentfour}</p>
+                <p>{page.titlefive}</p>
+                <p>{page.contentfive}</p>
+                <p className='page'>{page.content}</p>
+            </article>
+            ))}
+            </FlipPage>
+            </div>
         </div>
     )
 }
--- src/pages/QuestionsOne/QuestionOne.js
+++ src/pages/QuestionsOne/QuestionOne.js
@@ -22,7 +22,6 @@ function QuestionOne(props) {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [currentAnswer, setCurrentAnswer] = useState('');
   const [answers, setAnswers] = useState([]);
-  const [answerInput, setAnswerInput] = useState('')
   const [showResults, setShowResults] = useState(false);
   const [error, setError] = useState('');
   // const [answersA, setAnswersA] = useState([1,2,3]);
@@ -72,30 +71,31 @@ function QuestionOne(props) {
   }
 
   const handleSubmit = async event => {
-    event.preventDefault()
+    // event.preventDefault()
     const batch = Firebase.db.batch()
     const userRef = Firebase.db.collection('users').doc(user.uid)
     try {
       // Create a new post doc with auto ID locally
-      const newAnswerRef = Firebase.db.collection('answers').doc()
+      const newAnswerRef = Firebase.db.collection('answersOne').doc()
       // add batch op to set post data including logged in uid
-      batch.set(newAnswerRef, {
-        title: answerInput,
+      const answer = {
+        questionOne: questions[0].question,
+        answerOne: answers[0].answer,
+        questionTwo: questions[1].question,
+        answerTwo: answers[1].answer,
+        questionsThree: questions[2].question,
+        answerThree: answers[2].answer,
         lastModified: firebase.firestore.FieldValue.serverTimestamp(),
         uid: user.uid,
-      })
-      // add batch op to update user posts array
-      batch.update(userRef, {
-        posts: firebase.firestore.FieldValue.arrayUnion(newAnswerRef.id),
-      })
-      // commit batch ops
+      }
+      batch.set(newAnswerRef, answer)
+  
       batch.commit().then(() => {
         console.log('Added new post', newAnswerRef.id)
       })
     } catch (error) {
       console.error('Error adding document: ', error)
     }
-    setAnswerInput('')
   }
   
 
@@ -202,16 +202,18 @@ function QuestionOne(props) {
         </h2>
         <br/>
           {/* {renderResultsData()} */}
+          
           {nextQuestion()}
+          {handleSubmit()}
       </div>
     )
   }else{
   return (
     <div className='container'>
-    <form
+    {/* <form
         onSubmit={handleSubmit}
         style={{ marginTop: '.5em', marginBottom: '.5em' }}
-    >
+    > */}
       <Progress total={questions.length} current={currentQuestion + 1} />
       <Question question={question.question} />
       {renderError()}
@@ -219,7 +221,7 @@ function QuestionOne(props) {
       <button className='btn btn-primary' onClick={next}>
         Confirm And Continue
       </button>
-    </form>
+    {/* </form> */}
     </div>
   );
   }
